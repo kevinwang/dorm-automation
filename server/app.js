9758e01daee5ab3fc9c45dec1f3c8abc50c04ebc
Kevin Wang <kevin@kevinwang.com>
2013-07-30 15:48:40 -0400

    Add PUT route to switch modules on/off.

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ function setupConnection() {
         console.log('Connection successful');
     });
 
+    client.setNoDelay();
+
     client.on('error', function(e) {
         console.log('Connection error: ' + e.code);
         console.log('Restarting mochad; retrying in 3 seconds...');
@@ -19,13 +21,36 @@ function setupConnection() {
     });
 }
 
+function sendX10Command(addr, value, callback) {
+    var cmd = 'pl ' + addr + ' ' + value;
+    console.log(cmd);
+    client.write(cmd + '\n', null, callback);
+}
+
 setupConnection();
 
 // Web server
 var app = express();
+app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
     res.send({hello: 'world'});
+});
+
+app.put('/api/modules/:addr', function(req, res) {
+    if (['on', 'off'].indexOf(req.body.value) != -1) { 
+        sendX10Command(req.params.addr, req.body.value, function(err) {
+            if (err) {
+                res.send(500);
+            }
+            else {
+                res.send(200);
+            }
+        });
+    }
+    else {
+        res.send(400);
+    }
 });
 
 app.listen(3000);
